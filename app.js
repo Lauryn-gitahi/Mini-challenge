2e9d5d63c541f0409d663b40d7d1590762e94613
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2020-06-19 04:58:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+.container {
+  width100%;
+  height:100px;
+  margin: 20px;
+  box-sizing: border-box;
+  display: flex;
+  border: 2px solid black;
+  font-size:12px;
+}
+
+.grid {
+  border-right: 2px solid red;
+  margin-right: 20px;
+}
+
+.grid-two {
+  border-right:  2px solid red;
+  margin-right: 20px;
+}
+
+.grid-three {
+  border-right: 2px solid red;
+  margin-right: 20px;
+}
+
+.grid-four {
+  border-right: 2px solid red;
+  margin-right: 20px;
+}
+
+.grid-five {
+  border-right: 2px solid red;
+  margin-right: 20px;
+}
